092cfd7b61c041809009caf3609449d28bbecc56
Viet.Nguyen <vietnq2001@gmail.com>
2025-04-02 20:59:32 +0700

    case study update 2/4/2025

--- src/informationManagement.java
+++ src/informationManagement.java
@@ -24,6 +24,7 @@ public class informationManagement extends information implements IinformationMa
                 informationArray.add(eachInfor);
             }
             br.close();
+            fr.close();
         } catch (Exception e){
             e.printStackTrace();
         }
@@ -54,36 +55,48 @@ public class informationManagement extends information implements IinformationMa
     }
 
     public String validatePhoneNumber(){
-        while (true){
-            String phoneNumber;
-            System.out.println("Enter the phone number of the person");
-            phoneNumber = new Scanner(System.in).nextLine();
-            if (phoneNumber.matches("[0-9]{10}"))
-                return phoneNumber;
-            else {
-                System.out.println("invalid phone number");
-            }
+        String phoneNumber;
+        System.out.println("Enter the phone number of the person");
+        phoneNumber = new Scanner(System.in).nextLine();
+        if (phoneNumber.matches("[0-9]{10}"))
+            return phoneNumber;
+        else {
+            System.out.println("invalid phone number");
+            validatePhoneNumber();
         }
+        return phoneNumber;
     }
 
     public String validateEmail(){
-        while (true){
-            String email;
-            System.out.println("Enter your email: ");
-            email = new Scanner(System.in).nextLine();
-            if (email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")){
-                return email;
-            } else {
-                System.out.println("invalid email");
-            }
+        String email;
+        System.out.println("Enter your email: ");
+        email = new Scanner(System.in).nextLine();
+        if (email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")){
+            return email;
+        } else {
+            System.out.println("invalid email");
+            validateEmail();
         }
+        return email;
+    }
+
+    public String validateDate(){
+        System.out.println("Enter your date of birth: ");
+        String dateOfBirth = new Scanner(System.in).nextLine();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        try{
+            Date date = dateFormat.parse(dateOfBirth);
+        } catch (Exception e){
+            System.out.println("Invalid date format");
+            validateDate();
+        }
+        return dateOfBirth;
     }
 
     @Override
     public void addInformation() {
         // input information.txt
         int lastID = autoID();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         System.out.println("Enter your name: ");
         String name = new Scanner(System.in).nextLine();
         System.out.println("Enter your gender: ");
@@ -94,15 +107,7 @@ public class informationManagement extends information implements IinformationMa
         String group = new Scanner(System.in).nextLine();
         System.out.println("Enter your address: ");
         String address = new Scanner(System.in).nextLine();
-        System.out.println("Enter your date of birth: ");
-        String dateOfBirth = new Scanner(System.in).nextLine();
-        // validate dateOfBirth
-        try{
-            Date date = dateFormat.parse(dateOfBirth);
-        } catch (Exception e){
-            e.printStackTrace();
-            System.out.println("Invalid date format");
-        }
+        String dateOfBirth = validateDate();
         String email = validateEmail() ;
         ArrayList<information> informationArray = new ArrayList<>();
         informationArray.add(new information(lastID, name, gender, phoneNumber, group, address, dateOfBirth, email));
@@ -141,6 +146,7 @@ public class informationManagement extends information implements IinformationMa
         System.out.println("Enter the id of the information you would like to update: ");
         int upid = new Scanner(System.in).nextInt();
         File file = new File("information.txt");
+        ArrayList<String> updateList = new ArrayList<>();
         try{
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
@@ -150,8 +156,8 @@ public class informationManagement extends information implements IinformationMa
                 int id = Integer.parseInt(dataLine[0]);
                 if (upid == id) {
                     // let user change all the information about that person then append into file with old id
-                    information oldInfor = new information(id, dataLine[1], dataLine[2], dataLine[3], dataLine[4],dataLine[5],dataLine[6],dataLine[7]);
-                    System.out.println("Are you sure you want to update the information?" + oldInfor); // double check
+                    information infor = new information(id, dataLine[1], dataLine[2], dataLine[3], dataLine[4],dataLine[5],dataLine[6],dataLine[7]);
+                    System.out.println("Are you sure you want to update the information?" + infor); // double check
                     System.out.println("1. Yes");
                     System.out.println("2. No");
                     int choice = new Scanner(System.in).nextInt();
@@ -175,12 +181,23 @@ public class informationManagement extends information implements IinformationMa
                             break;
                     }
                     information newInfor = new information(id, dataLine[1], dataLine[2], dataLine[3], dataLine[4],dataLine[5],dataLine[6],dataLine[7]);
-
-                    FileWriter fw = new FileWriter("information.txt", true);
-                    fw.write(newInfor.toString());
+//                    FileWriter fw = new FileWriter("information.txt", true);
+//                    fw.write(newInfor.toString());
+                    updateList.add(newInfor.toString());
+                } else {
+                    updateList.add(line);
                 }
-
             }
+            br.close();
+            fr.close();
+            FileWriter fw = new FileWriter("information.txt");
+            fw.write("");
+            fw.close();
+            FileWriter fw1 = new FileWriter("information.txt",true);
+            for (int i = 0; i < updateList.size(); i++) {
+                fw1.write(updateList.get(i) + "\n");
+            }
+            fw1.close();
 
         } catch (Exception e){
             e.printStackTrace();
@@ -190,12 +207,34 @@ public class informationManagement extends information implements IinformationMa
 
     @Override
     public void deleteInformation() {
-
+        // find the person
+        File file = new File("information.txt");
+        // if not found then add into new arraylist
+        // override all txt file with the infor in arraylist
     }
 
     @Override
     public void searchInformation() {
-
+        File file = new File("information.txt");
+        System.out.println("Enter the id of the information you would like to search: ");
+        int searchId = new Scanner(System.in).nextInt();
+        try{
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] dataLine = line.split(",");
+                int id = Integer.parseInt(dataLine[0]);
+                information infor = new information(id, dataLine[1], dataLine[2], dataLine[3], dataLine[4],dataLine[5],dataLine[6],dataLine[7]);
+                if (id == searchId) {
+                    System.out.println("person number "+ id + " is " + infor);
+                }
+            }
+            br.close();
+            fr.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
